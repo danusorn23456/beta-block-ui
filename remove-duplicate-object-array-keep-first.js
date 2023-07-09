277e45f87ea41dfb1eacd5220c7b343b8970bb1f
danusorn23456 <danusorn23456@gmail.com>
2023-07-09 11:03:28 +0700

    update algo

--- remove-duplicate-object-array-keep-first.js
+++ remove-duplicate-object-array-keep-first.js
@@ -1,4 +1,3 @@
 let objArr = [{ name: "robot", version: 1 }, { name: "robot", version: 2 }, { name: "human", version: 1 }]
-let uniqName = []
-let nonDupObjArr = objArr.filter((o, index) => objArr.findLastIndex(co => co.name === o.name) === index)
+let nonDupObjArr = objArr.filter((o, index) => objArr.findIndex(co => co.name === o.name) === index)
 console.log(nonDupObjArr)
